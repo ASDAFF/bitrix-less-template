988d957d150dd62803f3e93ec1fd95042ef7e3bb
skobka <skobka@skobka-pc>
2014-06-28 21:01:47 +0600

    Общее обновление шаблона

--- header.php
+++ header.php
@@ -39,13 +39,24 @@ $oTemplate->init();
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="brand" href="#">Project name</a>
+                    <a class="brand" href="/"><?= $oTemplate->getSiteInfo('NAME');?></a>
                     <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contact">Contact</a></li>
-                        </ul>
+                         <?$APPLICATION->IncludeComponent("bitrix:menu", "nav", Array(
+	"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+	"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+	"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+	"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+	"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+		0 => "",
+	),
+	"MAX_LEVEL" => "1",	// Уровень вложенности меню
+	"CHILD_MENU_TYPE" => "",	// Тип меню для остальных уровней
+	"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+	"DELAY" => "N",	// Откладывать выполнение шаблона меню
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+	),
+	false
+);?>
                     </div><!--/.nav-collapse -->
                 </div>
             </div>
