644468619cf1ee421bf43ca1a097b0a5615e8586
artem.soshnikov <r9122249017@gmail.com>
2014-07-24 16:57:12 +0600

    мелкие исправления

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Файл шапки шаблона
  * 
@@ -9,7 +8,6 @@
  * @website http://skobka.com
  * @license http://skobka.com/license.html
  */
-
 /*
  * Подключаем наш основной класс шаблона
  */
@@ -26,13 +24,13 @@ $oTemplate->init();
 ?><!DOCTYPE html>
 <html>
     <head>
-        <title><?=$APPLICATION->ShowTitle();?></title>
-        <? $APPLICATION->ShowHead();?>
+        <title><?= $APPLICATION->ShowTitle(); ?></title>
+        <? $APPLICATION->ShowHead(); ?>
         <meta name="viewport" content="width=device-width">
     </head>
     <body>
-        <? $APPLICATION->ShowPanel();?>
-        <div class="container">`
+        <? $APPLICATION->ShowPanel(); ?>
+        <div class="container">
             <div class="navbar navbar-inverse">
                 <div class="navbar-inner">            
                     <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -40,27 +38,39 @@ $oTemplate->init();
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="brand" href="/"><?= $oTemplate->getSiteInfo('NAME');?></a>
+                    <a class="brand" href="/"><?= $oTemplate->getSiteInfo('NAME'); ?></a>
                     <div class="nav-collapse collapse">
-                         <?$APPLICATION->IncludeComponent("bitrix:menu", "nav", Array(
-	"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
-	"MENU_CACHE_TYPE" => "A",	// Тип кеширования
-	"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-	"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-	"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-		0 => "",
-	),
-	"MAX_LEVEL" => "1",	// Уровень вложенности меню
-	"CHILD_MENU_TYPE" => "",	// Тип меню для остальных уровней
-	"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-	"DELAY" => "N",	// Откладывать выполнение шаблона меню
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-	),
-	false
-);?>
+                        <?php
+                        $APPLICATION->IncludeComponent("bitrix:menu", "nav", Array(
+                            "ROOT_MENU_TYPE" => "top", // Тип меню для первого уровня
+                            "MENU_CACHE_TYPE" => "A", // Тип кеширования
+                            "MENU_CACHE_TIME" => "3600", // Время кеширования (сек.)
+                            "MENU_CACHE_USE_GROUPS" => "Y", // Учитывать права доступа
+                            "MENU_CACHE_GET_VARS" => array(// Значимые переменные запроса
+                                0 => "",
+                            ),
+                            "MAX_LEVEL" => "1", // Уровень вложенности меню
+                            "CHILD_MENU_TYPE" => "", // Тип меню для остальных уровней
+                            "USE_EXT" => "N", // Подключать файлы с именами вида .тип_меню.menu_ext.php
+                            "DELAY" => "N", // Откладывать выполнение шаблона меню
+                            "ALLOW_MULTI_SELECT" => "N", // Разрешить несколько активных пунктов одновременно
+                                ), false
+                        );
+                        ?>
                     </div><!--/.nav-collapse -->
                 </div>
             </div>
         </div>
-      
+
         <div class="container">
+            <?php ob_start(); ?>
+            <?php 
+                /*
+                 * ob-функции добавлены для сохранения возможности 
+                 * автоформатирования кода в NetBeans 8.0
+                 */
+            ?>
+        </div>
+    </body>
+</html>
+<?php ob_get_clean(); ?>
